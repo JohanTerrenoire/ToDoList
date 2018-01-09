f265afb05884a275228b5f264c74448902a0d375
JohanTerrenoire <terrenoirejohan@yahoo.fr>
2018-01-09 12:49:58 +0100

    Ajout gestion des erreurs

--- script.js
+++ script.js
@@ -9,13 +9,24 @@ function envoyerDonnees(){
 
 //Fonction de suppression des items sélectionnés
 function supprimerDonnees(checkbox){
-  var xhr = new XMLHttpRequest();
-  for (var i=0; i<checkbox.length;i++) { //Parcours du formulaire
-    if (checkbox[i].checked == true) { //Si la checkbox est cochée
-      var url = "supprimer.php?chk_id=" + checkbox[i].id;
-      xhr.open("GET", url);
-      xhr.send(null);
-      document.getElementById(checkbox[i].id).remove();
+  var chk_sel = false;
+  for (var i=0; i<checkbox.length;i++) {
+    if (checkbox[i].checked == true) { //Si il y a au moins un élément sélectionné
+      chk_sel = true;
+    }
+  }
+  if (chk_sel == false) { //Si aucun élément est sélectionné, message d'erreur
+    alert("Vous n'avez sélectionné aucun élément.");
+  }
+  else {
+    var xhr = new XMLHttpRequest();
+    for (var i=0; i<checkbox.length;i++) { //Parcours du formulaire
+      if (checkbox[i].checked == true) { //Si la checkbox est cochée
+        var url = "supprimer.php?chk_id=" + checkbox[i].id;
+        xhr.open("GET", url);
+        xhr.send(null);
+        document.getElementById(checkbox[i].id).remove();
+      }
     }
   }
 }
